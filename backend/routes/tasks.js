f884d9c6059d86823a17d09fa1666d90f0797973
Ryan Davey <ryandavey@college.harvard.edu>
2019-11-08 09:32:06 -0500

    uploading backend files

---
+++ backend/routes/tasks.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+let Task = require('../models/task.model');
+
+router.route('/').get((req, res) => {
+  Task.find()
+    .then(tasks => res.json(tasks))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const title = req.body.title;
+
+  const newTask = new Task({title});
+
+  newTask.save()
+    .then(() => res.json('Task added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Task.findById(req.params.id)
+    .then(task => res.json(task))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Task.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Task deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Task.findById(req.params.id)
+    .then(task => {
+      task.title = req.body.title;
+
+      task.save()
+        .then(() => res.json('Task updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
